e4f7321ee4f07234b1c79c087913d4d77a6ba655
www-data <www-data@files.ling.umd.edu>
2009-08-30 01:36:32 -0400

    Add parseqstring.pm module.

---
+++ parseqstring.pm
@@ -0,0 +1,59 @@
+package parseqstring;
+
+use warnings;
+use strict;
+
+require Exporter;
+
+use vars qw(@ISA @EXPORT @EXPORT_OK);
+use common;
+
+# Returns a (reference to a) list of strings which the user wants to find
+# (ignores AND, OR and terms preceded by -).
+sub parse_qstring {
+    my $qstring = shift;
+
+    my $inquote = 0;
+    my $inminus = 0;
+    my $currentword = "";
+    my @words;
+
+    my $resetvs = sub { # Doing it this way so it closes over outer vars
+        push @words, $currentword if ! $inminus;
+        $currentword = "";
+    };
+
+    for (my $i = 0; $i < length($qstring); ++$i) {
+        my $c = substr($qstring,$i,1);
+        if ($c eq '"' || $c eq "'") {
+            &$resetvs;
+            $inminus = 0 if $inquote;
+            $inquote = ! $inquote;
+        }
+        elsif ($c eq "-") {
+            &$resetvs;
+            $inminus = 1;
+            $inquote = 0;
+        }
+        elsif ($c =~ /\s/) {
+            if ($inquote) {
+                $currentword .= $c;
+            }
+            else {
+                &$resetvs;
+                # Leaving $inminus alone...
+            }
+        }
+        else {
+            $currentword .= $c;
+        }
+    }
+    push @words, $currentword if ! $inminus;
+
+    return \@words;
+}
+
+@ISA = qw( Exporter Autoloader );
+@EXPORT = qw( parse_qstring );
+
+1;
