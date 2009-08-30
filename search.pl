71965109a2ee8ed38047cdb3fc73f7ddf6cb5d09
root <root@files.ling.umd.edu>
2009-08-30 00:10:53 -0400

    Various improvements.

--- search.pl
+++ search.pl
@@ -4,10 +4,11 @@ use strict;
 
 use SWISH::API;
 use common;
-use File::Spec::Functions;
+use File::Spec::Functions qw(catfile catdir splitpath);
 use PDF;
 use Encode qw(decode);
 use Encode::Guess;
+use JSON;
 
 sub guessenc {
     my $s = shift(@_);
@@ -60,19 +61,29 @@ for (my $i = 0;
     my $author = "";
     my $subject = "";
     if ($name && $name =~ /\.pdf$/ && (! $qs_hash->{no_meta})) {
-	my $pdf;
-	eval {
-            $pdf = new PDF($f);
-	};
-	if (! $@) {
-            $numpages = $pdf->Pages || "";
-            $title = guessenc($pdf->GetInfo("Title")) || "";
-            $author = guessenc($pdf->GetInfo("Author")) || "";
-            $subject = guessenc($pdf->GetInfo("Subject")) || "";
-	}
+        my $cachefilename = catfile(METADATA_CACHE_DIR, $name) . '.mdcache';
+        if (-f $cachefilename) {
+            my $json;
+            eval {
+                local undef $/;
+                open J, $cachefilename;
+                $json = <J>;
+                close J;
+            };
+            if (! $@) {
+                my $mdata = JSON::from_json($json);
+                if (ref($mdata) eq "HASH") {
+                    $numpages = $mdata->{numpages};
+                    $title = $mdata->{title};
+                    $author = $mdata->{author};
+                    $subject = $mdata->{subject};
+                }
+            }
+        }
     }
 
-    push @rlist, { 'url' => $f, 'name' => $name, 'short_name' => trunc($name), 'numpages' => $numpages, 'title' => $title, 'author' => $author, 'subject' => $subject };
+    my ($vol, $dir, $fname) = splitpath($name);
+    push @rlist, { 'url' => $f, 'name' => $name, 'short_name' => $qs_hash->{no_meta} ? $name : trunc($name), 'dir_url' => $dir, 'numpages' => $numpages, 'title' => $title, 'author' => $author, 'subject' => $subject };
 }
 print $cgi->header(-status => "200 OK", -type => 'text/html', -encoding => 'utf-8');
 $tt->process('results.html', { 'url_prefix' => URL_PREFIX, 'query_url' => QUERY_URL, 'results' => \@rlist, 'query' => $query, 'meta' => ($qs_hash->{no_meta} ? 0 : 1), 'subtitle' => "results for $query" }) || die $tt->error;
